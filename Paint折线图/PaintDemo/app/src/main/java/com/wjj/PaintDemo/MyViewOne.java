65d9953971df7156f6f05e0c200409cc675822e1
王亟亟 <526357367@qq.com>
2016-04-18 16:36:02 +0800

    折线图例子

---
+++ Paint折线图/PaintDemo/app/src/main/java/com/wjj/PaintDemo/MyViewOne.java
@@ -0,0 +1,101 @@
+package com.wjj.PaintDemo;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.os.Handler;
+import android.os.Message;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by jiajiewang on 16/4/18.
+ */
+public class MyViewOne extends View{
+    private int XPoint = 120;
+    private int YPoint = 520;
+    private int XScale = 16; // 刻度长度
+    private int YScale = 80;
+    private int XLength = 760;
+    private int YLength = 480;
+
+    private int MaxDataSize = XLength / XScale;
+
+    private List<Integer> data = new ArrayList<Integer>();
+
+
+
+    private String[] YLabel = new String[YLength / YScale];
+
+    private Handler handler = new Handler(){
+        public void handleMessage(Message msg) {
+            if(msg.what == 0x1234){
+                MyViewOne.this.invalidate();
+            }
+        };
+    };
+    public MyViewOne(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        for(int i=0; i<YLabel.length; i++){
+            YLabel[i] = (i + 1) + "M/s";
+        }
+
+        new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                while(true){
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    if(data.size() >= MaxDataSize){
+                        data.remove(0);
+                    }
+                    data.add(new Random().nextInt(4) + 1);
+                    handler.sendEmptyMessage(0x1234);
+                }
+            }
+        }).start();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        Paint paint = new Paint();
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setAntiAlias(true); //去锯齿
+        paint.setColor(Color.BLUE);
+        paint.setTextSize(30);
+
+        //画Y轴
+        canvas.drawLine(XPoint, YPoint - YLength, XPoint, YPoint, paint);
+
+        //Y轴箭头
+        canvas.drawLine(XPoint, YPoint - YLength, XPoint - 3, YPoint-YLength + 6, paint);  //箭头
+        canvas.drawLine(XPoint, YPoint - YLength, XPoint + 3, YPoint-YLength + 6 ,paint);
+
+        //添加刻度和文字
+        for(int i=0; i * YScale < YLength; i++) {
+            canvas.drawLine(XPoint, YPoint - i * YScale, XPoint + 5, YPoint - i * YScale, paint);  //刻度
+
+            canvas.drawText(YLabel[i], XPoint -80, YPoint - i * YScale, paint);//文字
+        }
+
+        //画X轴
+        canvas.drawLine(XPoint, YPoint, XPoint + XLength, YPoint, paint);
+        System.out.println("Data.size = " + data.size());
+        if(data.size() > 1){
+            for(int i=1; i<data.size(); i++){
+                canvas.drawLine(XPoint + (i-1) * XScale, YPoint - data.get(i-1) * YScale,
+                        XPoint + i * XScale, YPoint - data.get(i) * YScale, paint);
+            }
+        }
+    }
+}
